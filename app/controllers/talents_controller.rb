de6e084225b22bad1b99a02a2e9f924aaa532d78
jvelis94 <joelvelis94@gmail.com>
2021-07-08 22:24:41 -0400

    added filters on left hand side

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -1,5 +1,7 @@
 class TalentsController < ApplicationController
     def index
+        @business_units = ["Infrastructure", "Institutional Sales & Trading", "IT & Networking", "Management", "Quantitative Research", "Technology", "Trading"]
+        @teams = ["Equities", "ETFs", "Finance & Treasury", "Fixed Income & Commodities", "Intern", "Legal & Compliance", "Management", "Operations", "Options", "Software Development", "Software Engineering"]
         if params['filter'].present?
             puts params['filter']
             @talents = Talent.where(business_unit: params['filter'])
