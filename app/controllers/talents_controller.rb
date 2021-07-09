0be8c60cc97fa4f17136eb02260809af6d393a85
jvelis94 <joelvelis94@gmail.com>
2021-07-09 12:28:48 -0400

    implemented filter results to reflect on index page

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -5,22 +5,34 @@ class TalentsController < ApplicationController
         @teams = Talent.distinct.pluck(:team).sort
         @sub_teams = Talent.distinct.pluck(:sub_team).sort
         @entities = Talent.distinct.pluck(:entity).sort
-        @leadership = ["Leadership"]
         if params['filter'].present?
-            locations = params['filter']['locations'].reject(&:blank?)
-            leadearship = params['filter']['leadership'].reject(&:blank?)
-            business_units = params['filter']['business_units'].reject(&:blank?)
-            teams = params['filter']['teams'].reject(&:blank?)
-            sub_teams = params['filter']['sub_teams'].reject(&:blank?)
-            entities = params['filter']['entities'].reject(&:blank?)
+            locations = params['filter']['locations'].present? ? params['filter']['locations'].reject(&:blank?) : nil
+            leadership = params['filter']['leadership'].reject(&:blank?) ? params['filter']['leadership'].reject(&:blank?) : nil
+            business_units = params['filter']['business_units'] ? params['filter']['business_units'].reject(&:blank?) : nil
+            teams = params['filter']['teams'] ? params['filter']['teams'].reject(&:blank?) : nil
+            sub_teams = params['filter']['sub_teams'] ? params['filter']['sub_teams'].reject(&:blank?) : nil
+            entities = params['filter']['entities'] ? params['filter']['entities'].reject(&:blank?) : nil
 
+            @talents = Talent.all
+            @talents = @talents.where(location: locations) if locations.length > 0
+            @talents = @talents.where(leadership: leadership) if leadership.length > 0
+            @talents = @talents.where(business_unit: business_units) if business_units.length > 0
+            @talents = @talents.where(team: teams) if teams.length > 0
+            @talents = @talents.where(sub_team: sub_teams) if sub_teams.length > 0
+            @talents = @talents.where(entity: entities) if entities.length > 0
             
+            puts @talents.count
             # query = URI.parse(request.url).query
             # query = query.gsub("%20", " ").gsub("filter=", "").split(",")
             # @talents = Talent.where(business_unit: query)
             # render partial: 'talents_list', locals: { talents: @talents }, layout: false
         else
             @talents = Talent.all
+        end
+
+        respond_to do |format|
+            format.html
+            format.js
         end
     end
 end
