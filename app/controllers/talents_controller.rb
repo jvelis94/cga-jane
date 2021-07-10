305736b28264259c2e12592419ad19d2d87b0c94
jvelis94 <joelvelis94@gmail.com>
2021-07-10 12:04:04 -0400

    split teams into jse and jsc

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -2,22 +2,33 @@ class TalentsController < ApplicationController
     def index
         @locations = Talent.distinct.pluck(:location).sort
         @business_units = Talent.distinct.pluck(:business_unit).sort
-        @teams = Talent.distinct.pluck(:team).sort
+        # @capital_teams = Talent.distinct.pluck(:team).sort
+        @capital_teams = ["ADRs", "Equities", "ETFs", "Finance & Treasury", "Finance & Treasury", "Fixed Income & Commodities","Hardware Design",
+            "Institutitonal Strategy & Business Development","Intern", "Legal & Compliance", "Market Data", "Marketing", "Operations",
+            "Options", "Other", "Regulatory Capital & Risk", "Software Development", "Software Engineering", "Surveillance", "Trading Desk Operations"
+        ]
+
+        @execution_teams = ["Client Onboarding", "Equities Sales & Trading", "ETF Sales and Trading", "Finance & Treasury",
+            "Fixed Income Sales & Trading", "Institutitonal Strategy & Business Development", "Legal & Compliance", "Operations",
+            "Options Sales & Trading", "Other", "Software Development", "Software Engineering", "Trading Desk Operations"
+        ]
         @sub_teams = Talent.distinct.pluck(:sub_team).sort
         @entities = Talent.distinct.pluck(:entity).sort
         if params['filter'].present?
             locations = params['filter']['locations'].present? ? params['filter']['locations'].reject(&:blank?) : nil
-            leadership = params['filter']['leadership'].reject(&:blank?) ? params['filter']['leadership'].reject(&:blank?) : nil
-            business_units = params['filter']['business_units'] ? params['filter']['business_units'].reject(&:blank?) : nil
-            teams = params['filter']['teams'] ? params['filter']['teams'].reject(&:blank?) : nil
-            sub_teams = params['filter']['sub_teams'] ? params['filter']['sub_teams'].reject(&:blank?) : nil
-            entities = params['filter']['entities'] ? params['filter']['entities'].reject(&:blank?) : nil
+            leadership = params['filter']['leadership'].present? ? params['filter']['leadership'].reject(&:blank?) : nil
+            business_units = params['filter']['business_units'].present? ? params['filter']['business_units'].reject(&:blank?) : nil
+            capital_teams = params['filter']['capital_teams'].present? ? params['filter']['capital_teams'].reject(&:blank?) : nil
+            execution_teams = params['filter']['execution_teams'].present? ? params['filter']['execution_teams'].reject(&:blank?) : nil
+            sub_teams = params['filter']['sub_teams'].present? ? params['filter']['sub_teams'].reject(&:blank?) : nil
+            entities = params['filter']['entities'].present? ? params['filter']['entities'].reject(&:blank?) : nil
 
             @talents = Talent.all
             @talents = @talents.where(location: locations) if locations.length > 0
             @talents = @talents.where(leadership: leadership) if leadership.length > 0
             @talents = @talents.where(business_unit: business_units) if business_units.length > 0
-            @talents = @talents.where(team: teams) if teams.length > 0
+            @talents = @talents.where(team: capital_teams) if capital_teams.length > 0
+            @talents = @talents.where(team: execution_teams) if execution_teams.length > 0
             @talents = @talents.where(sub_team: sub_teams) if sub_teams.length > 0
             @talents = @talents.where(entity: entities) if entities.length > 0
             @talent_ids = @talents.pluck(:id)
